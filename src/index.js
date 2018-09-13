3c160159c7894d094a9ffff4a76e8f9d7f976169
Maksim Astravukh <astravukh@gmail.com>
2018-09-13 22:22:23 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    var length = str.length;
+    for(var i = 0; i < bracketsConfig.length; i++ ){
+      str = str.replace(bracketsConfig[i][0] + bracketsConfig[i][1], "")
+    }
+    if(length == str.length){
+      return false;
+    }
+
+    if(str.length == 0){
+      return true;
+    }
+    else{
+      return check(str, bracketsConfig);
+    }
 }
